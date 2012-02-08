cea6b4e161c27e53d44783eb128472c9ffef4900
Gunnar Sv Sigurbjörnsson <gunnars10@ru.is>
2012-02-08 13:29:46 +0000

    Test

--- Bootstrap/bootstrap.py
+++ Bootstrap/bootstrap.py
@@ -1,10 +1,20 @@
-from twisted.internet import protocol, reactor
+from twisted.internet import reactor, protocol
 
 class Bootstrap(protocol.Protocol):
-    def dataReceived(self, data):
-        self.transport.write(data)
+    def __init__(self):
+        self.clients = []
 
+    def connectionMade(self):
+        print "connection from: ", self.transport.getPeer().host
+        self.clients.append(self.transport.getPeer().host)
+                
+    def dataReceived(self, data):
+        #self.transport.write(data)
+        print "from: ", self.transport.getPeer().host,":",self.transport.getPeer().port, " : ", data
+
+
+bootstrapPort = 8007
 factory = protocol.ServerFactory()
 factory.protocol = Bootstrap
-reactor.listenTCP(8007, factory)
+reactor.listenTCP(bootstrapPort, factory)
 reactor.run()
