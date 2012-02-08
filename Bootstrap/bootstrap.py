43a40ef0b4a8579dad93832b6712adf24b70c643
Ymir Vigfusson <ymir@ru.is>
2012-02-08 14:44:18 +0000

    Improvements to bootstrap and client

--- Bootstrap/bootstrap.py
+++ Bootstrap/bootstrap.py
@@ -1,16 +1,61 @@
 from twisted.internet import reactor, protocol
+import pickle
 
 class Bootstrap(protocol.Protocol):
     def __init__(self):
-        self.clients = []
+        self.clients = {}
 
     def connectionMade(self):
         print "connection from: ", self.transport.getPeer().host
-        self.clients.append(self.transport.getPeer().host)
-                
+        #self.clients.append(self.transport.getPeer().host)
+	c = "%s:%d" % (self.transport.getPeer().host, self.transport.getPeer().port)
+        self.clients[c] = { "host" : self.transport.getPeer().host, "port" : self.transport.getPeer().port}
+
+    ### XXX hvad ef tengingin slitnar?
+
+    def sendPickle(self, cmd, data):
+	self.transport.write ("%c%s" % (cmd, pickle.dumps(data)))
+
+    def recvPickle(self, data):
+	try:
+		obj = pickle.loads(data[1:])
+		print obj
+		self.transport.write("TACK\n")
+		return obj
+	except:
+		print "Error parsing pickle command"
+		self.transport.write("TERR\n")
+		return None
+   
+    def sendNodes(self, c):
+	# Send a list of relevant nodes to a particular client
+	self.sendPickle('L', [ "192.168.1.1", "192.168.1.2" ])
+ 
     def dataReceived(self, data):
         #self.transport.write(data)
-        print "from: ", self.transport.getPeer().host,":",self.transport.getPeer().port, " : ", data
+        #print "from: ", self.transport.getPeer().host,":",self.transport.getPeer().port, " : ", data
+	# Protocol, 'T' means text, 'P' means pickle
+	c = "%s:%d" % (self.transport.getPeer().host, self.transport.getPeer().port)
+	if data[0] == 'T': # Text
+		print data[1:]
+	elif data[0] == "N":
+		obj = self.recvPickle(data)
+		# XXX error handling
+		self.clients[c]['inport'] = obj['inport']
+		self.clients[c]['rate'] = obj['rate']
+	elif data[0] == "C": ## Content
+		obj = self.recvPickle(data)
+		# XXX error handling
+		self.clients[c]['interests'] = obj['interests']
+		print "Interests: ", self.clients[c]['interests']
+	elif data[0] == "G":
+		obj = self.recvPickle(data)
+		# Client wants a list of nodes
+		self.sendNodes (c)	
+	else:
+		self.transport.write("TNo such command\n")
+		
+	
 
 
 bootstrapPort = 8007
--- Client/client.py
+++ Client/client.py
@@ -1,13 +1,68 @@
 from twisted.internet import protocol, reactor
+import os
+import pickle
 
 class Client(protocol.Protocol):
+    STATE_INITIAL = 1
+    STATE_SENT_N = 2
+    STATE_SENT_C = 3
+    STATE_SENT_G = 4
+
     def __init__(self):
         self.connectTo = ""
+	self.state = self.STATE_INITIAL
 
+
+    def sendContent(self):
+   	# Protocol: Step 1: Send information about self
+	me = { "interests" : [ "red little riding hood", "vertigo" ] }
+	self.transport.write ("C%s" % (pickle.dumps(me)))
+ 
+    def sendNew(self):
+	# Protocol: Step 1: Send information about self
+	me = { "inport" : "1234", "pid" : str(os.getpid()), "rate" : [ 1,2,3,4] }
+	self.transport.write ("N%s" % (pickle.dumps(me)))
+	
+    def sendGetNodes(self):
+	# Protocol: Step 1: Send information about self
+	self.transport.write ("G")
+	
     def dataReceived(self, data):
         self.connectTo = data
+
+	#print "from: ", self.transport.getPeer().host,":",self.transport.getPeer().port, " : ", data
+	# Protocol, 'T' means text, 'P' means pickle
+	if data[0] == 'T': # Text
+		if data[1:4] == "ACK": # Success
+			if self.state == self.STATE_SENT_N:
+				self.sendContent()
+				self.state = self.STATE_SENT_C
+
+			if self.state == self.STATE_SENT_C:
+				self.sendGetNodes()
+				self.state = self.STATE_SENT_G
+
+		print data[1:]
+	elif data[0] == 'L': ## List of nodes
+		try:
+			obj = pickle.loads(data[1:])
+			print obj
+			# XXX: do something with the object
+			#self.transport.write("TACK\n")
+		except:
+			print "Error parsing pickle command"
+			self.transport.write("TERR\n")
+	else:
+		self.transport.write("TUnknown response\n")
+	
         print data
-        
+       
+    def connectionMade(self):
+    	self.sendNew()
+	self.state = self.STATE_SENT_N
+
+
+ 
 
 bootstrap = "localhost"
 bootstrapPort = 8007
